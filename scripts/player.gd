cdfc1b3abe5fe38a33f4ea53d4e3778fd199524e
Shoyur129 <patrick.alaniz16@gmail.com>
2026-05-04 17:25:03 -0500

    edited player movement and kz
    added a kill zone at bottom of world and added player animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,10 +4,11 @@ extends CharacterBody2D
 const SPEED = 150.0
 const JUMP_VELOCITY = -350.0
 
-var health := 5
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-func _ready() -> void:
-	add_to_group("player")
+@onready var animated_sprite = $AnimatedSprite2D
+
+var health := 5
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -15,12 +16,25 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	# flip the sprite based on movement direction
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+
+	# Set the animation based on movement and state
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("Idle")
+		else:
+			animated_sprite.play("Run")
+	else:
+		animated_sprite.play("Jump")
+
 	if direction:
 		velocity.x = direction * SPEED
 	else:
