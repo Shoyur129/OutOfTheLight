c5da9d58b11f63b2d856d57a78ccd55b76b4bb77
Shoyur129 <patrick.alaniz16@gmail.com>
2026-05-06 14:50:08 -0500

    updates
    remade kill zone, updated level 1 with kz. game now restarts when falling into void

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -2,11 +2,9 @@ extends Area2D
 
 @onready var timer = $Timer
 
-func _on_body_entered(_body):
-	print ("You died")
-	Engine.time_scale = 0.5
+func _on_body_entered(_body: Node2D) -> void:
+	print("you died")
 	timer.start()
 
 func _on_timer_timeout():
-	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
--- scripts/player.gd
+++ scripts/player.gd
@@ -20,10 +20,10 @@ func _physics_process(delta: float):
 		return
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 		if direction < 0:
