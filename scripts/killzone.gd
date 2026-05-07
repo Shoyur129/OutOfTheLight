d360cb896f8ee8df1b3a7208cb49702ed8607b2f
Shoyur129 <patrick.alaniz16@gmail.com>
2026-05-06 19:32:43 -0500

    Playable level
    mostly finished the first part of the level. added spikes, player animations decorated the environment

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -3,8 +3,14 @@ extends Area2D
 @onready var timer = $Timer
 
 func _on_body_entered(_body: Node2D) -> void:
-	print("you died")
-	timer.start()
+	if _body.is_in_group("player"):
+		_body.is_dead = true
+		_body.velocity = Vector2.ZERO
+		if _body.sprite.sprite_frames.has_animation("Die"):
+			_body.sprite.play("Die")
+		Engine.time_scale = 0.5
+		timer.start()
 
 func _on_timer_timeout():
+	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
